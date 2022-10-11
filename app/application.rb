3255971c6cb0ad75faecaa06373d4787538ff018
Janan Akkad <jananakkad@hotmail.co.uk>
2022-10-11 10:04:30 +0100

    commit tree 59599671691c96b9eda7767096060bbcb4a731fc

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,37 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    num_1 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
+
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+
+    if num_1==num_2 && num_2==num_3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
   end
 
 end
+
+#visit http://localhost:9292/ to see the web app. The 9292 is generated after running:
+# rackup config.ru in the command line. It represents the port number which may change with your location/device etc. 
+
+
+## BASIC RACK APP: 
+# class Application
+
+#   def call(env)
+#     resp = Rack::Response.new
+#     resp.write "Hello, World"
+#     resp.finish
+#   end
+
+# end
